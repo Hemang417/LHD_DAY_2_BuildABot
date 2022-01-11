545c3f54a981b1593318977c14160fd9659ecf27
Hemang Ranjan <74303960+Hemang417@users.noreply.github.com>
2022-01-11 13:25:18 +0530

    Added best of functionality
    Implemented the play_best_of function for the user to play a number of times

--- main.py
+++ main.py
@@ -10,9 +10,9 @@ def play():
         return "Its a Tie"
 
     if is_win(user, computer):
-        return "You Win !"
+        return (1, user, computer)
 
-    return "You Lost !"
+    return (-1, user, computer)
 
 def is_win(player, opponent):
     # winning conditions: r > s, s > p, p > r
@@ -22,12 +22,23 @@ def is_win(player, opponent):
 
 def play_best_of(n):
     # play against the computer until someone wins the best of n games
-
     player_win = 0
     computer_win = 0
     wins_necessary = math.ceil(n/2)
-    print(wins_necessary)
+    while player_win < wins_necessary or computer_win < wins_necessary:
+        result, user, computer = play()
+        #tie
+        if(result==0):
+            print("Its a Tie.")
+        #you win
+        elif(result==1):
+            player_win += 1
+            print("You Won")
+        else:
+            computer_win+=1
+            print("Computer Won")
+        print("\n")
 
 if __name__=='__main__':
     play_best_of(2)
-    play_best_of(5)
+    # play_best_of(5)
